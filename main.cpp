da759681fce4a8988c4721bf94bc3de3699ca62b
Cristian Aragón <crisarag8.sal@gmail.com>
2023-10-31 08:09:22 -0600

    Función para verificar la jugada del usuario v1

--- main.cpp
+++ main.cpp
@@ -14,6 +14,31 @@ vector<int> generarPatronAleatorio(int longitud, int numColores) {
     return patron;
 }
 
+// Función para verificar la jugada del usuario
+bool verificarJugada(const vector<int>& patronSecreto, const vector<int>& jugada) {
+    int longitud = patronSecreto.size();
+    if (patronSecreto.size() != jugada.size()) {
+        return false;
+    }
+    
+    int aciertos = 0;
+    int coincidencias = 0;
+    for (int i = 0; i < longitud; i++) {
+        if (patronSecreto[i] == jugada[i]) {
+            aciertos++;
+        } else {
+            for (int j = 0; j < longitud; j++) {
+                if (patronSecreto[i] == jugada[j]) {
+                    coincidencias++;
+                    break;
+                }
+            }
+        }
+    }
+    cout << "Aciertos: " << aciertos << " Coincidencias: " << coincidencias << endl;
+    return aciertos == longitud;
+}
+
 int main() {
     
 }
