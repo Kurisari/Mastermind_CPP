91a504d52d9fafd62e38bb5edca8af94914b2802
Cristian Aragón <crisarag8.sal@gmail.com>
2023-10-31 08:22:12 -0600

    Mostrar aciertos añadido

--- main.cpp
+++ main.cpp
@@ -14,19 +14,33 @@ vector<int> generarPatronAleatorio(int longitud, int numColores) {
     return patron;
 }
 
-// Función para verificar la jugada del usuario
+vector<int> obtenerPosicionesAciertos(const vector<int>& patronSecreto, const vector<int>& jugada) {
+    vector<int> posicionesAciertos;
+    for (int i = 0; i < patronSecreto.size(); i++) {
+        if (patronSecreto[i] == jugada[i]) {
+            posicionesAciertos.push_back(i);
+        }
+    }
+    return posicionesAciertos;
+}
+
 bool verificarJugada(const vector<int>& patronSecreto, const vector<int>& jugada) {
     int longitud = patronSecreto.size();
     if (patronSecreto.size() != jugada.size()) {
         return false;
     }
-    
-    int aciertos = 0;
+    vector<int> posicionesAciertos = obtenerPosicionesAciertos(patronSecreto, jugada);
+    int aciertos = posicionesAciertos.size();
     int coincidencias = 0;
     for (int i = 0; i < longitud; i++) {
-        if (patronSecreto[i] == jugada[i]) {
-            aciertos++;
-        } else {
+        bool aciertoEnPosicion = false;
+        for (int j = 0; j < posicionesAciertos.size(); j++) {
+            if (posicionesAciertos[j] == i) {
+                aciertoEnPosicion = true;
+                break;
+            }
+        }
+        if (!aciertoEnPosicion) {
             for (int j = 0; j < longitud; j++) {
                 if (patronSecreto[i] == jugada[j]) {
                     coincidencias++;
@@ -36,6 +50,13 @@ bool verificarJugada(const vector<int>& patronSecreto, const vector<int>& jugada
         }
     }
     cout << "Aciertos: " << aciertos << " Coincidencias: " << coincidencias << endl;
+    if (aciertos > 0) {
+        cout << "Posiciones acertadas: ";
+        for (int i = 0; i < aciertos; i++) {
+            cout << posicionesAciertos[i] << " ";
+        }
+        cout << endl;
+    }
     return aciertos == longitud;
 }
 
@@ -48,21 +69,21 @@ int main() {
     cin >> modoJuego;
     if (modoJuego == 1) {
         vector<int> patronSecreto;
-        cout << "Crea un patrón secreto de " << longitud << " elementos usando números del 1 al " << numColores << endl;
+        cout << "Crea un patron secreto de " << longitud << " elementos usando numeros del 1 al " << numColores << endl;
         for (int i = 0; i < longitud; i++) {
             int color;
             cin >> color;
             patronSecreto.push_back(color);
         }
-        cout << "Patrón secreto establecido. Ahora es turno de la computadora." << endl;
+        cout << "Patron secreto establecido. Ahora es turno de la computadora." << endl;
         vector<int> jugadaComputadora;
         while (!verificarJugada(patronSecreto, jugadaComputadora)) {
             jugadaComputadora = generarPatronAleatorio(longitud, numColores);
         }
-        cout << "¡La computadora adivinó el patrón!" << endl;
+        cout << "¡La computadora adivino el patron!" << endl;
     } else if (modoJuego == 2) {
         vector<int> patronSecreto = generarPatronAleatorio(longitud, numColores);
-        cout << "La computadora ha creado un patrón secreto. Intenta adivinarlo." << endl;
+        cout << "La computadora ha creado un patron secreto. Intenta adivinarlo." << endl;
         vector<int> jugadaUsuario;
         while (true) {
             cout << "Ingresa tu jugada de " << longitud << " elementos usando números del 1 al " << numColores << endl;
@@ -73,14 +94,14 @@ int main() {
                 jugadaUsuario.push_back(color);
             }
             if (verificarJugada(patronSecreto, jugadaUsuario)) {
-                cout << "¡Adivinaste el patrón secreto!" << endl;
+                cout << "¡Adivinaste el patron secreto!" << endl;
                 break;
             } else {
-                cout << "Inténtalo de nuevo." << endl;
+                cout << "Intentalo de nuevo." << endl;
             }
         }
     } else {
-        cout << "Modo de juego no válido. Debe ser 1 o 2." << endl;
+        cout << "Modo de juego no valido. Debe ser 1 o 2." << endl;
     }
     return 0;
 }
